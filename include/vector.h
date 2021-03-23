34cf031731ea0c1c54ba45e391249b30335e44e8
Joe Nelson <joe@begriffs.com>
2021-03-22 23:28:02 -0500

    Add const where possible

--- include/vector.h
+++ include/vector.h
@@ -14,14 +14,14 @@ typedef struct vector
 
 vector * v_new(void (*elt_dtor)(void *));
 void     v_free(vector *);
-size_t   v_length(vector *);
+size_t   v_length(const vector *);
 bool     v_set_length(vector *, size_t);
-size_t   v_capacity(vector *);
+size_t   v_capacity(const vector *);
 size_t   v_reserve_capacity(vector *, size_t);
-bool     v_is_empty(vector *);
-void *   v_at(vector *, size_t);
-void *   v_first(vector *);
-void *   v_last(vector *);
+bool     v_is_empty(const vector *);
+void *   v_at(const vector *, size_t);
+void *   v_first(const vector *);
+void *   v_last(const vector *);
 bool     v_append(vector *, void *);
 bool     v_prepend(vector *, void *);
 void *   v_remove_first(vector *);
@@ -29,9 +29,11 @@ void *   v_remove_last(vector *);
 bool     v_insert(vector *, size_t, void *);
 bool     v_remove(vector *, size_t i, size_t len);
 bool     v_swap(vector *, size_t, size_t);
-bool     v_clear(vector *);
-size_t   v_find_index(vector *, void *, int (*)(const void *, const void *));
-size_t   v_find_last_index(vector *, void *, int (*)(const void *, const void *));
+void     v_clear(vector *);
+size_t   v_find_index(const vector *, void *,
+                      int (*)(const void *, const void *));
+size_t   v_find_last_index(const vector *, void *,
+                           int (*)(const void *, const void *));
 bool     v_sort(vector *, int (*)(const void *, const void *));
 
 #endif
