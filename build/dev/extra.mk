2d509e028b41e1290b264f0f80b758699f2ee214
Joe Nelson <joe@begriffs.com>
2021-04-26 22:29:02 -0500

    Generate code coverage properly

--- build/dev/extra.mk
+++ build/dev/extra.mk
@@ -1,4 +1,3 @@
 CC = clang
-CFLAGS += -std=c99 -pedantic -Wall -Wextra -Werror -O0 -g
-LDFLAGS += -fprofile-instr-generate -fcoverage-mapping \
-           -fsanitize=address -fsanitize=undefined
+CFLAGS += -std=c99 -pedantic -Wall -Wextra -Werror -O0 -g --coverage
+LDFLAGS += -fsanitize=address -fsanitize=undefined -fsanitize=leak
